79b1d37059765825d25c68d772a4a3921c9f002d
ANDREYTOLOGY <63473361+ANDREYTOLOGY@users.noreply.github.com>
2020-04-11 02:30:44 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const cartButton = document.querySelector("#cart-button");
+comst modal = document.querySelector(.modal");
+cartButton.addEventListener('click', function (event) {
+    modal.classList.add("is-open");
+});
